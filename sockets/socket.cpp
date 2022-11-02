8d9040197f7387cedc37f852549ff47d6dae9522
AntoninLiens <antoninliens@hotmail.be>
2022-11-02 10:16:13 +0100

    socket.hpp

--- sockets/socket.cpp
+++ sockets/socket.cpp
@@ -6,12 +6,89 @@
 /*   By: aliens <aliens@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 16:06:34 by aliens            #+#    #+#             */
-/*   Updated: 2022/11/01 16:08:26 by aliens           ###   ########.fr       */
+/*   Updated: 2022/11/02 10:13:46 by aliens           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "socket.hpp"
 
-tcpSocket::tcpSocket() {}
+tcpSocket::tcpSocket(int domain, int port) {
+	this->_addr.sin_family = domain;
+	this->_addr.sin_addr.s_addr = INADDR_ANY;
+	this->_addr.sin_port = htons(port);
+	this->_addrlen = sizeof(this->_addr);
+}
 
 tcpSocket::~tcpSocket() {}
+
+
+
+bool	tcpSocket::initSocket(int domain, int type, int protocol)
+{
+	if ((this->_socket = socket(domain, type, protocol)))
+		return (true);
+	return (false);
+}
+
+void	tcpSocket::closeSocket() { close(this->_socket); }
+
+
+
+bool	tcpSocket::bindSocket(int domain, int port)
+{
+	if (!bind(this->_socket, (const sockaddr *)&this->_addr, this->_addrlen) < 0)
+		return (true);
+	throw (tcpSocket::errorSocket().sendError());
+}
+
+bool	tcpSocket::listenSocket()
+{
+	if (!listen(this->_socket, SOMAXCONN))
+		return (true);
+	throw (tcpSocket::errorSocket().listenError());
+}
+
+
+
+bool	tcpSocket::connectSocket(int domain, const std::string &addr, int port)
+{
+	sockaddr_in	srv;
+	inet_pton(domain, addr.c_str(), &srv.sin_addr.s_addr);
+	srv.sin_family = domain;
+	srv.sin_port = htons(port);
+	
+	return (!(connect(this->_socket, (const sockaddr *)&srv, sizeof(srv))));
+}
+
+bool	tcpSocket::acceptSocket(client *client)
+{
+	if (!(client->_cli._socket = accept(this->_socket, (sockaddr *)&this->_addr, &this->_addrlen)))
+		return (true);
+	throw (tcpSocket::errorSocket().acceptError());
+}
+
+
+
+int		tcpSocket::recvSocket(char *buffer, unsigned int len)
+{
+	int	ret = recv(this->_socket, buffer, len, 0);
+	if (ret == -1)
+		throw (tcpSocket::errorSocket().recvError());
+	return (ret);
+}
+
+int		tcpSocket::sendSocket(const char *data, unsigned int len)
+{
+	int ret = send(this->_socket, data, len, 0);
+	if (ret == -1)
+		throw(tcpSocket::errorSocket().sendError());
+	return (ret);
+}
+
+
+
+const char	*tcpSocket::errorSocket::bindError() const throw() { return ("socket: error: bind"); }
+const char	*tcpSocket::errorSocket::recvError() const throw() { return ("socket: error: recv"); }
+const char	*tcpSocket::errorSocket::sendError() const throw() { return ("socket: error: send"); }
+const char	*tcpSocket::errorSocket::listenError() const throw() { return ("socket: error: listen"); }
+const char	*tcpSocket::errorSocket::acceptError() const throw() { return ("socket: error: accept"); }
--- sockets/socket.hpp
+++ sockets/socket.hpp
@@ -6,27 +6,59 @@
 /*   By: aliens <aliens@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 16:02:19 by aliens            #+#    #+#             */
-/*   Updated: 2022/11/01 16:06:18 by aliens           ###   ########.fr       */
+/*   Updated: 2022/11/02 10:13:07 by aliens           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SOCKET_HPP
+#define SOCKET_HPP
+
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
-class tcpSocket {
-private:
-	int	_socket;
-
-public:
-	tcpSocket();
-	~tcpSocket();
-	
-};
+#include <iostream>
 
 struct client
 {
-	tcpSocket	cli_sock;
-	sockaddr_in	addr;
+	tcpSocket	_cli;
+	sockaddr_in	_srv;
 };
 
+class tcpSocket {
+private:
+	int			_socket = 0;
+	sockaddr_in	_addr = {0};
+	socklen_t	_addrlen = 0;
+	
+public:
+	tcpSocket(int domain = AF_INET, int port = 8080);
+	~tcpSocket();
+
+	bool	initSocket(int domain, int type, int protocol);
+	void	closeSocket();
+
+	bool	bindSocket(int domain, int port);
+	bool	listenSocket();
+	
+	bool	connectSocket(int domain, const std::string &addr, int port);
+	bool	acceptSocket(client *client);
+
+	int		recvSocket(char *buffer, unsigned int len);
+	int		sendSocket(const char *data, unsigned int len);
+
+	class errorSocket : public std::exception {
+	public:
+		virtual const char	*bindError() const throw();
+		virtual const char	*recvError() const throw();
+		virtual const char	*sendError() const throw();
+		virtual const char	*listenError() const throw();
+		virtual const char	*acceptError() const throw();
+		
+	};
+	
+};
+
+#endif
