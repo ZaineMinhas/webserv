f41ab0aa8192c4813c11215bcb76373ab7735019
ZaineMinhas <zaineminhas5@gmail.com>
2023-01-03 21:01:45 +0100

    delete et image site sont plus presentable

--- website/site/images.php
+++ website/site/images.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-
 <html>
 	<head>
 		<meta charset="utf-8"/>
 		<title>Delete cgi</title>
-		<link rel="stylesheet" href="./stylesheet/delete.css"/>
+		<link rel="stylesheet" href="./stylesheet/images.css"/>
 	</head>
 	<body>
 		<?php
@@ -13,7 +11,7 @@
 					$files = scandir('./website/site/uploads');
 					foreach ($files as $file)
 						if (!($file == "." || $file == ".."))
-							echo "<a href='/delete.py?file=$file'><img src='./uploads/$file'/></a><br/>";
+							echo "<a href='./uploads/$file'><img src='./uploads/$file'/></a>";
 				}
 				else
 					echo "<div>There is no images.</div>";
